d1667b4b40f3857157bb1477ca3c95fb48eb7b97
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-05-22 16:18:08 +0100

    removed deck only using shoe and card

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ import cards.Shoe;
 public class App {
     public static void main(String[] args) throws Exception {
         Shoe shoe = new Shoe(8);
-        System.out.println(shoe.getNumCards());
+        System.out.println(shoe.getFirstCards(10));
     }
 }
--- src/cards/Shoe.java
+++ src/cards/Shoe.java
@@ -1,20 +1,49 @@
 package cards;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Shoe {
-    private int numDecks;
+    private ArrayList<Card> cards;
 
     public Shoe(int numDecks) {
         if (numDecks < 1 || numDecks > 100) {
-            throw new IllegalArgumentException("numDecks must be between 1 and 10");
-        }
-        for (int d = 0; d < numDecks; d++) {
-            new Deck();
+            throw new IllegalArgumentException("numDecks must be between 1 and 100");
         }
 
-        this.numDecks = numDecks;
+        cards = new ArrayList<>();
+        String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+
+        for (int d = 0; d < numDecks; d++) {
+            for (int value = 1; value <= 13; value++) {
+                for (String suit : suits) {
+                    cards.add(new Card(value, suit));
+                }
+            }
+        }
+
+        Collections.shuffle(cards);
     }
 
     public int getNumCards() {
-        return numDecks * 52;
+        return cards.size();
+    }
+
+    public String getFirstCards(int limit) {
+        if (limit < 1 || limit > cards.size()) {
+            throw new IllegalArgumentException("Limit out of bounds");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < limit; i++) {
+            sb.append(cards.get(i).toString());
+        }
+        return sb.toString();
+    }
+
+    public Card draw() {
+        if (cards.isEmpty())
+            throw new IllegalStateException("No more cards in the shoe!");
+        return cards.remove(0);
     }
 }
